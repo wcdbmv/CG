07d0a51a3d9426bf9db276b018472812c195bf2c
Kerimov Ahmed <kerimov.dev@yandex.ru>
2019-03-14 20:22:52 +0300

    pain_t

--- lab03/mainwindow.cpp
+++ lab03/mainwindow.cpp
@@ -2,17 +2,23 @@
 #include "ui_mainwindow.h"
 
 #include <QColorDialog>
+#include <cmath>
 
 MainWindow::MainWindow(QWidget *parent) :
 	QMainWindow(parent),
 	ui(new Ui::MainWindow),
-	fgColor(Qt::black),
-	bgColor(Qt::white)
+	fgColor(defaultFgColor),
+	image(QImage(720, 720, QImage::Format_ARGB32_Premultiplied)),
+	scene(new QGraphicsScene(0, 0, 720, 720))
 {
 	ui->setupUi(this);
 
 	colorLabel(ui->fgLabel, fgColor);
-	colorLabel(ui->bgLabel, bgColor);
+
+	image.fill(defaultBgColor);
+
+	ui->graphicsView->setScene(scene);
+	imageLabel();
 }
 
 MainWindow::~MainWindow()
@@ -28,24 +34,305 @@ void MainWindow::colorLabel(QLabel *label, QColor &color)
 	label->setPalette(palette);
 }
 
-void MainWindow::on_fgPushButton_clicked()
-{
-	QColor color = QColorDialog::getColor(fgColor, this, "Pick a FG color", QColorDialog::DontUseNativeDialog);
-	colorLabel(ui->fgLabel, color);
+void MainWindow::imageLabel() {
+	scene->clear();
+	scene->addPixmap(QPixmap::fromImage(image));
+	repaint();
 }
 
-void MainWindow::on_bgPushButton_clicked()
+template <typename T> int sgn1(T val) {
+	return val >= 0 ? 1 : -1;
+}
+
+// Процедура разложения в растр отрезка по методу цифрового дифференциального анализатора (ЦДА)
+void MainWindow::dda(const QLine &line)
 {
-	QColor color = QColorDialog::getColor(bgColor, this, "Pick a BG color", QColorDialog::DontUseNativeDialog);
-	colorLabel(ui->bgLabel, color);
+	const int deltaX = line.p2().x() - line.p1().x();
+	const int deltaY = line.p2().y() - line.p1().y();
+
+	int length = qMax(qAbs(deltaX), qAbs(deltaY));
+
+	// Предполагается, что концы отрезка не совпадают
+	if (!length) {
+		image.setPixel(line.p1().x(), line.p1().y(), fgColor.rgb());
+		return;
+	}
+
+	// Полагаем большее из приращений dx или dy равным единице растра
+	const double dx = (double) deltaX / length;
+	const double dy = (double) deltaY / length;
+
+	// Округляем величины, а не отбрасываем дробную часть
+	// Использование знаковой функции делает алгоритм пригодным для всех квадрантов
+	double x = line.p1().x() + 0.5 * sgn1(dx);
+	double y = line.p1().y() + 0.5 * sgn1(dy);
+
+	// Начало основного цикла
+	while (length--) {
+		image.setPixel(qRound(x), qRound(y), fgColor.rgb());
+		x += dx;
+		y += dy;
+	}
+}
+// Анализ отрезков, проведенных из точки (0, 0) в точку (—8, 4) и (8, —4), показывает, что
+// разложенный в растр отрезок лежит по одну сторону от реального и что на одном из концов отрезка
+// появляется лишняя точка, т. е. результат работы алгоритма зависит от ориентации. Следовательно,
+// точность в концевых точках ухудшается. Далее, если вместо взятия целой части использовать
+// округление до ближайшего целого, то результаты снова получатся разными. Таким образом, либо
+// нужно использовать более сложный и более медленный алгоритм, либо надо отступиться от требования
+// максимально точной аппроксимации. Вдобавок предложенный алгоритм имеет тот недостаток, что он
+// использует вещественную арифметику.
+
+
+// Алгоритм Брезенхема разложения в растр отрезка
+//
+// Алгоритм выбирает оптимальные растровые координаты для представления отрезка. В процессе работы
+// одна из координат — либо x, либо y (в зависимости от углового коэффициента) — изменяется на
+// единицу. Изменение другой координаты (либо на нуль, либо на единицу) зависит от расстояния между
+// действительным положением отрезка и ближайшими координатами сетки. Такое расстояние мы назовем
+// ошибкой.
+void MainWindow::bresenhamFloat(const QLine &line)
+{
+	// Предполагается, что концы отрезка не совпадают
+	if (line.p1() == line.p2()) {
+		image.setPixel(line.p1().x(), line.p1().y(), fgColor.rgb());
+		return;
+	}
+
+	// Инициализация переменных
+	double x = line.p1().x();
+	double y = line.p1().y();
+	int dx = line.p2().x() - line.p1().x();
+	int dy = line.p2().y() - line.p1().y();
+	const int sx = sgn1(dx);
+	const int sy = sgn1(dy);
+	dx = qAbs(dx);
+	dy = qAbs(dy);
+	const double m = (double) dy / dx;
+
+	// обмен значений dx и dy в зависимости от углового коэффициента наклона отрезка
+	const bool swapped = dy > dx;
+	if (swapped)
+		qSwap(dx, dy);
+
+	// Инициализация e с поправкой на половину пиксела
+	double e = m - 0.5;
+
+	// Начало основного цикла
+	for (int i = 0; i != dx; ++i) {
+		image.setPixel(x, y, fgColor.rgb());
+		if (e >= 0.0) {
+			if (swapped)
+				x += sx;
+			else
+				y += sy;
+			--e;
+		}
+		else {
+			if (swapped)
+				y += sy;
+			else
+				x += sx;
+			e += m;
+		}
+	}
+}
+
+// Целочисленный алгоритм Брезенхема
+void MainWindow::bresenhamInteger(const QLine &line)
+{
+	// Предполагается, что концы отрезка не совпадают
+	if (line.p1() == line.p2()) {
+		image.setPixel(line.p1().x(), line.p1().y(), fgColor.rgb());
+		return;
+	}
+
+	// Инициализация переменных
+	int x = line.p1().x();
+	int y = line.p1().y();
+	int dx = line.p2().x() - line.p1().x();
+	int dy = line.p2().y() - line.p1().y();
+	const int sx = sgn1(dx);
+	const int sy = sgn1(dy);
+	dx = qAbs(dx);
+	dy = qAbs(dy);
+
+	// обмен значений dx и dy в зависимости от углового коэффициента наклона отрезка
+	const bool swapped = dy > dx;
+	if (swapped)
+		qSwap(dx, dy);
+
+	const int dx2 = 2 * dx;
+	const int dy2 = 2 * dy;
+
+	// Инициализация e с поправкой на половину пиксела
+	int e = dy2 - dx;
+
+	// Начало основного цикла
+	for (int i = 0; i != dx; ++i) {
+		image.setPixel(x, y, fgColor.rgb());
+		if (e >= 0.0) {
+			if (swapped)
+				x += sx;
+			else
+				y += sy;
+			e -= dx2;
+		}
+		else {
+			if (swapped)
+				y += sy;
+			else
+				x += sx;
+			e += dy2;
+		}
+	}
+}
+
+void MainWindow::bresenhamAntialiased(const QLine &line)
+{
+	if (line.p1() == line.p2()) {
+		image.setPixel(line.p1().x(), line.p1().y(), fgColor.rgb());
+		return;
+	}
+
+	const int i_max = 255;
+	int dx = line.p2().x() - line.p1().x();
+	int dy = line.p2().y() - line.p1().y();
+	int sx = sgn1(dx);
+	int sy = sgn1(dy);
+	dx = qAbs(dx);
+	dy = qAbs(dy);
+	double x = line.p1().x();
+	double y = line.p1().y();
+
+	const bool swapped = dy > dx;
+	if (swapped)
+		qSwap(dx, dy);
+
+	double m = static_cast<double>(i_max * dy) / dx;
+	double e = i_max / 2.0;
+	double w = i_max - m;
+	double alpha = i_max;
+	QColor color = fgColor;
+	for (int i = 0; i != dx; ++i) {
+		color.setAlpha(alpha);
+		image.setPixel(x, y, color.rgba());
+		alpha = e;
+		if (e <= w) {
+			if (swapped)
+				y += sy;
+			else
+				x += sx;
+			e += m;
+		}
+		else {
+			x += sx;
+			y += sy;
+			e -= w;
+		}
+	}
+}
+
+void MainWindow::defaultQt(const QLine &line)
+{
+
+}
+
+static inline int ipart(double x) { return floor(x); }
+static inline double fpart(double x) { return x - floor(x); }
+static inline double rfpart(double x) { return 1 - fpart(x); }
+
+void MainWindow::wu(const QLine &line)
+{
+	if (line.p1() == line.p2()) {
+		// image.setPixel(line.p1().x(), line.p1().y(), fgColor.rgb());
+		return;
+	}
+
+	int x1 = line.p1().x();
+	int y1 = line.p1().y();
+	int x2 = line.p2().x();
+	int y2 = line.p2().y();
+
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+
+	const bool swapped = qAbs(dx) < qAbs(dy);
+	if (swapped) {
+		qSwap(x1, y1);
+		qSwap(x2, y2);
+		qSwap(dx, dy);
+	}
+	if (x2 < x1) {
+		qSwap(x1, x2);
+		qSwap(y1, y2);
+	}
+
+	dx = x2 - x1;
+	dy = y2 - y1;
+	double grad = dx ? static_cast<double>(dy) / dx : 1;
+
+	double y = y1;
+	for (int x = x1; x <= x2; ++x) {
+		QColor color = fgColor;
+		if (swapped) {
+			color.setAlphaF(rfpart(y));
+			image.setPixel(ipart(y), x, color.rgba());
+			color.setAlphaF(fpart(y));
+			image.setPixel(ipart(y) - 1, x, color.rgba());
+		}
+		else {
+			color.setAlphaF(rfpart(y));
+			image.setPixel(x, ipart(y), color.rgba());
+			color.setAlphaF(fpart(y));
+			image.setPixel(x, ipart(y) - 1, color.rgba());
+		}
+		y += grad;
+	}
+}
+void MainWindow::on_fgPushButton_clicked()
+{
+	fgColor = QColorDialog::getColor(fgColor, this, "Pick a FG color", QColorDialog::DontUseNativeDialog);
+	colorLabel(ui->fgLabel, fgColor);
+}
+
+void MainWindow::on_setBGColorToFGPushButton_clicked()
+{
+	fgColor = defaultBgColor;
 }
 
 void MainWindow::on_drawLinePushButton_clicked()
 {
-	//
+	const int x1 = ui->x1SpinBox->text().toInt();
+	const int y1 = ui->y1SpinBox->text().toInt();
+	const int x2 = ui->x2SpinBox->text().toInt();
+	const int y2 = ui->y2SpinBox->text().toInt();
+	const QLine line(x1, y1, x2, y2);
+
+	if (ui->ddaRadioButton->isChecked())
+		dda(line);
+	else if (ui->bresenhamFloatRadioButton->isChecked())
+		bresenhamFloat(line);
+	else if (ui->bresenhamIntegerRadioButton->isChecked())
+		bresenhamInteger(line);
+	else if (ui->bresenhamAntialiasedRadioButton->isChecked())
+		bresenhamAntialiased(line);
+	else if (ui->defaultQtRadioButton->isChecked())
+		defaultQt(line);
+	else if (ui->wuRadioButton->isChecked())
+		wu(line);
+	else
+		Q_ASSERT(0);
+
+	imageLabel();
 }
 
 void MainWindow::on_drawSunPushButton_clicked()
 {
 	//
+}
+
+void MainWindow::on_clearAllPushButton_clicked()
+{
+	fgColor = defaultFgColor;
 }
--- lab03/mainwindow.h
+++ lab03/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QLabel>
+#include <QGraphicsScene>
 
 namespace Ui {
 class MainWindow;
@@ -18,17 +19,31 @@ public:
 
 private:
 	void colorLabel(QLabel *label, QColor &color);
+	void imageLabel();
+
+	void dda(const QLine &line);
+	void bresenhamFloat(const QLine &line);
+	void bresenhamInteger(const QLine &line);
+	void bresenhamAntialiased(const QLine &line);
+	void defaultQt(const QLine &line);
+	void wu(const QLine &line);
 
 private slots:
 	void on_fgPushButton_clicked();
-	void on_bgPushButton_clicked();
+	void on_setBGColorToFGPushButton_clicked();
 	void on_drawLinePushButton_clicked();
 	void on_drawSunPushButton_clicked();
+	void on_clearAllPushButton_clicked();
 
 private:
 	Ui::MainWindow *ui;
 
-	QColor fgColor, bgColor;
+	QColor fgColor;
+	const QColor defaultBgColor = QColor(Qt::darkGray);
+	const QColor defaultFgColor = QColor(Qt::red);
+
+	QImage image;
+	QGraphicsScene *scene;
 };
 
 #endif // MAINWINDOW_H
